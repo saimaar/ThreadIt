fd3dedc6947c9258671c532a8b820c2b22f6c189
saimaar <rahman.saima92@gmail.com>
2020-04-04 15:33:08 -0400

    css fix wedding blog

--- src/Components/Home/Wedding.jsx
+++ src/Components/Home/Wedding.jsx
@@ -11,20 +11,21 @@ class Wedding extends Component {
   render() {
     const src = 'https://react.semantic-ui.com/images/wireframe/image-text.png'
     return (
-      <Segment className ="wed-look">
+      <div className ="wed-look">
+<section>
+<p className="wed-look-book">Wedding Look Book</p>
+<p className="wed-look-book">We can create any look for you! Book an apointment now!</p>
+  <Image src={wed1} size='xlarge' centered />
+  <p className="wed1-ptag">
+      Wedding is the biggest moment of our lives!
+      It surely comes with a lot stress, planning etc etc.....
+      Let us help you with your wedding dress. Customize and pick any desgin
+      from our collection.
+  </p>
+</section>
 
-        <h1 className="look-book">Wedding Look Book</h1>
-        <h2>We can create any look for you! Book an apointment now!</h2>
-        <Image src={wed1} size='xlarge' centered />
-
-        <p className="wed1-ptag">
-            Wedding is the biggest moment of our lives!
-            It surely comes with a lot stress, planning etc etc.....
-            Let us help you with your wedding dress. Customize and pick any desgin
-            from our collection.
-        </p>
-
-    <Image className="wed3-img" src={wed2} size='large' left />
+<section>
+    <Image className="wed3-img" src={wed2} size='large'/>
     <p className="wed2-ptag">
       While earlier this was a smaller store,
       we have now completely revamped it to make a super
@@ -32,20 +33,14 @@ class Wedding extends Component {
       Apart from lehengas, they also have some nice jacket silhouettes,
       and a lot of Eastern and Western inspired outfits.
       They are pretty afforable.
-
     </p>
-
-    <p className="wed3-ptag">
-      Each and every detail are handcrafted by our amazing workers! It takes 3 to 6
-      months to create a perfect bridal look. Be it Summer, Winter or Spring Wedding,
-      we are here to help you design your special wedding dress.
-    </p>
-
-    <p className="wed4-ptag">
-      Email us at threadit@gmail.com<br/>
-      Contact : 222-999-3333
-    </p>
-  </Segment>
+</section>
+      <p className="wed3-ptag">
+        Each and every detail are handcrafted by our amazing workers! It takes 3 to 6
+        months to create a perfect bridal look. Be it Summer, Winter or Spring Wedding,
+        we are here to help you design your special wedding dress.
+      </p>
+  </div>
     );
   }
 
