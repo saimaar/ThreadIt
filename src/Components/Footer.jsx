7b72d8e7bde001295115f6d5aea76c2086155535
saimaar <rahman.saima92@gmail.com>
2020-04-04 15:13:44 -0400

    added link to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,18 @@
 import React, { PureComponent } from 'react';
-import {Header, Image, Segment} from 'semantic-ui-react'
+import {Icon, Segment, Button} from 'semantic-ui-react'
+import {Link} from 'react-router-dom';
+
 
 class Footer extends PureComponent {
 
   render() {
     return (
-      <div>this is footer</div>
+
+    <Segment id="footer">
+     <Button href="https://youtu.be/qzQDJL2lFHw" circular color='youtube' icon='youtube' />
+     <Button href="https://www.linkedin.com/in/saimi1992/" circular color='linkedin' icon='linkedin' />
+     <Button href="https://github.com/saimaar/threadIt-Frontend" circular color="black" icon='github' />
+    </Segment>
     );
   }
 
