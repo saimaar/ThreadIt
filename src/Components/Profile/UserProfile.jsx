96073a9f3d26790dacb4a03282dda062ffd1ebcb
saimaar <rahman.saima92@gmail.com>
2020-04-05 14:34:50 -0400

    added css userprofile

--- src/Components/Profile/UserProfile.jsx
+++ src/Components/Profile/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Card, Image, Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
+import {Header,Card, Image, Button, Divider, Form, Grid, Segment, Menu } from 'semantic-ui-react'
 import {saveUserToState} from '../../Redux/Actions/userActions'
 import { withRouter } from 'react-router-dom'
 import {Link} from 'react-router-dom'
@@ -25,26 +25,26 @@ class UserProfile extends Component {
 
     return (
 
-      <Segment className="prof">
-        <Grid className="prof" columns={2}>
-          <Grid.Column>
             <Segment id="profile-info">
-              <h3>Username:</h3>
-              <h4>{username ? username.slice(0,1).toUpperCase() + username.slice(1) : null}</h4>
-              <h3> email:</h3>
-              <h4>{email}</h4>
-              <Button onClick={this.handleDelete} basic color='red' content='Delete Account'/>
+              <Grid className="prof">
+              <Grid.Column floated='left' width={3} >
+              <p>Username:</p>
+              <p>{username ? username.slice(0,1).toUpperCase() + username.slice(1) : null}</p>
+              <p> email:</p>
+              <p>{email}</p>
+              </Grid.Column>
+              <Divider/>
+
+            <Grid.Column floated='right' width={3}>
+            <Button onClick={this.handleDelete} basic color='red' content='Delete Account'/>
+            <Link to={'./orders'}><Button content='order history' icon='unordered list' size='big' /></Link>
+            </Grid.Column>
+            </Grid>
+
             </Segment>
 
-          </Grid.Column>
 
-          <Grid.Column verticalAlign='middle'>
-            <Link to={'./orders'}><Button content='order history' icon='unordered list' size='big' /></Link>
-          </Grid.Column>
-        </Grid>
 
-        <Divider vertical>or</Divider>
-      </Segment>
 
 
     );
@@ -60,12 +60,3 @@ const mapStateToProps=(state)=>{
 }
 
 export default connect(mapStateToProps, {saveUserToState})(withRouter(UserProfile));
-
-//
-// <div>
-//   <h2>Username: {username}</h2>
-//   <h2>email: {email}</h2>
-//   <Button onClick={this.handleDelete} basic color='red' content='Delete Account'/>
-//
-//
-// </div>
